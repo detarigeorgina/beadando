62ecf2f61f710b464ada551653c3ccb693198d74
farsang.mateű <farsang.mate@ckik.loc>
2023-04-18 07:59:20 +0200

    ismetles kijavitva

--- generalas.py
+++ generalas.py
@@ -1,12 +1,19 @@
 from random import randint
 asd = []
+magasok = []
 
 for j in range(500):
+    magasok.append(randint(140,195))
+
+while len(asd) < 500:
+
     azonosito = ""
     for i in range(10):
         
         azonosito += chr(randint(65, 90))
-    asd.append(azonosito)
+    if azonosito not in asd:
+        asd.append(azonosito)
+    pass
 
 fw = open("asdd.txt", "w")
 for i in range(len(asd)):
