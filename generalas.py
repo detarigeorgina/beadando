7b89664369c24c2792e0fdbb3650a904cd652867
farsang.mateű <farsang.mate@ckik.loc>
2023-04-18 08:28:09 +0200

    mördzsöltünk tanárúr egy fálylt

--- generalas.py
+++ generalas.py
@@ -1,14 +1,23 @@
 from random import randint
 asd = []
+magasok = []
+lm = []
 
 for j in range(500):
+    magasok.append(randint(140,195))
+    lm.append(randint(12, 49))
+
+while len(asd) < 500:
+
     azonosito = ""
     for i in range(10):
         
         azonosito += chr(randint(65, 90))
-    asd.append(azonosito)
+    if azonosito not in asd:
+        asd.append(azonosito)
+    pass
 
 fw = open("asdd.txt", "w")
 for i in range(len(asd)):
-    fw.write(asd[i]+"\n")
+    fw.write(asd[i]+" "+str(magasok[i])+" "+  str(lm[i]) + "\n")
 fw.close()
